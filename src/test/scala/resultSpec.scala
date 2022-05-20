90c4914d7cb7100ca6bfe2eebc903ab4134b1ef8
DeclanRoutledgeBBC <90246178+DeclanRoutledgeBBC@users.noreply.github.com>
2022-05-20 18:47:27 +0100

    TicTacToe:v2 Adding failing result tests to check for a no win on empty board and horizontal win row 2 not picking up the first empty row as a win
    Also adding gitignore

--- src/test/scala/resultSpec.scala
+++ src/test/scala/resultSpec.scala
@@ -5,6 +5,13 @@ class resultSpec extends AnyFlatSpec {
   val winningGame = StockGames.horizontalWinGame()
   assert(Result.winChecker(winningGame) ==="Winner: O")}
 
+  it should "not result in a win given an empty board" in {
+    assert(Result.winChecker(board()) ==="Winner: O")}
+
+  it should "result in a win given an horizontal win of second row" in {
+    val winningGame = StockGames.horizontalWinGame(1)
+    assert(Result.winChecker(winningGame)==="Winner: O")}
+
   it should "not result in draw if the game is not finished" in{
     assert(Result.drawChecker(board())=== "Game not finished yet")
   }
