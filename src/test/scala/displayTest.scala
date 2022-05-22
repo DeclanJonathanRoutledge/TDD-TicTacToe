29ead646fffea0067db625fd58de109cd6790eb4
DeclanRoutledgeBBC <90246178+DeclanRoutledgeBBC@users.noreply.github.com>
2022-05-22 22:29:33 +0100

    Adding a terminal based board viewer

---
+++ src/test/scala/displayTest.scala
@@ -0,0 +1,26 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import StockGames.drawGame
+
+class displayTest extends AnyFlatSpec {
+
+  behavior of "Display"
+
+  it should "format an empty board" in {
+    print(display(board()).viewBoard)
+      //assert(display(board()).viewBoard == " ")
+  }
+
+  it should "format a full board" in {
+    assert(display(drawGame).viewBoard ==
+        " O || X || O \n" +
+        "===||===||===\n" +
+        " X || O || X \n" +
+        "===||===||===\n" +
+        " X || O || X \n")
+  }
+
+  it should "rowFormatter" in {
+
+  }
+
+}
